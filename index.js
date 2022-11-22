fcafc5f266f5ab1338cf6e4dc0a99e04ee59f5b6
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-11-22 21:19:37 +0300

    Completed Assignement

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 =31;
+let num2=2;
+
+let multiply =parseInt(num1, 10) * parseInt(num2, 10);
+
+
+
+let num3=19;
+let num4=5;
+let mod = num3%num4;
+
+const max = Math.max(12,15,7,20);
+
+let random = Math.ceil(Math.random()*10+1);
